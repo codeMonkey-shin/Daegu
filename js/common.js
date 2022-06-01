b2e1a6fc037cb5c326c675c5f1c4add321c20477
DongYeopLee <103983410+dong7314@users.noreply.github.com>
2022-06-01 18:00:38 +0900

    mainpage jquery
    mainpage jquery

--- js/common.js
+++ js/common.js
@@ -1,3 +1,32 @@
 $(function(){
+  // header
+
+  // gnb 호버 시
+  $('.gnb-hover').css('display', 'none');
+  $('#gnb li, .gnb-hover').hover(function(){
+    $('.gnb-hover').slideDown(300);
+  });
+  $('main').hover(function(){
+    $('.gnb-hover').slideUp(300);
+  })
+
+  // 로그인 랭귀지 클릭
+  $('.util-wrap .login .lst').css('display', 'none');
+  $('.util-wrap .language .lst').css('display', 'none');
+  $('.util-wrap > ul > li').click(function(){
+    if ( $('.lst' ,this).css('display') == 'block' ) {
+      $('.lst' ,this).css('display', 'none');
+    } else {
+      $('.util-wrap > ul .lst').css('display', 'none');
+      $('.lst' ,this).css('display', 'block');
+    }
+  });
+  $('html').click(function(e) {
+  	if(!$(e.target).hasClass("notClickArea")) {
+      $('.util-wrap .login .lst').css('display', 'none');
+      $('.util-wrap .language .lst').css('display', 'none');
+  	}
+  });
+
 
 });
